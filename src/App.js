88fd8adffa860576831dc8a2ca0c7143fb0f053e
bradley vangelder <r0784535@student.thomasmore.be>
2021-01-16 15:25:30 +0100

    animations added on intro and navigation, now fixing deifferent sections

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
+import React, {useState} from 'react';
 import './App.css';
-import Nav from './components/navigation/navigation';
 
 function App() {
+
+  const [show, setShow] = useState('close');
+
   return (
     <>
-      <Nav />
+      <h1 className="animation_text animation">Bradley Vangelder</h1>
+
+
+      <h1 className="nav_name">Bradley Vangelder</h1>
+
+      <div className="nav">
+          <ul className={show !== 'close' ? 'slide_left' : 'slide_back'}>
+              <li><a href="/#" onClick={(e) => setShow('about')}>about</a></li>
+              {console.log(show)}
+              <li><a href="/#" onClick={(e) => setShow('contact')}>contact</a></li>
+              <li><a href="/#" onClick={(e) => setShow('resume')}>resume</a></li>
+          </ul>
+      </div>
+
+      <div className={show === 'about' ? 'slide_left_container container': 'none'}>
+        <h2>About</h2>
+        <p>Wat text eh boykes</p>
+      </div>
     </>
   );
 }
