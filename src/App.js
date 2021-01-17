de245b2c1cc869ef0e1da9e7ca6677f5fcf97221
bradley vangelder <r0784535@student.thomasmore.be>
2021-01-17 14:10:17 +0100

    git ignore

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import React, {useState} from 'react';
 import './App.css';
 import sea from "./resources/sea.mp4";
+import Container from 'react-bootstrap/Container'
 
 function App() {
 
   const [show, setShow] = useState('close');
 
   return (
-    <>
-
+    <Container>
       <video autoPlay loop muted id="videoBG">
         <source src={sea} type="video/mp4"/>
       </video>
 
       <h1 className="animation_text animation">Bradley Vangelder</h1>
-      
+
       <h1 className="nav_name">Bradley Vangelder</h1>
 
       <div className="nav">
           <ul className={show !== 'close' ? 'slide_left' : 'slide_back'}>
-              <li><a href="/#" onClick={(e) => setShow('about')}>about</a></li>
+              <li onClick={(e) => setShow('about')}>about</li>
               {console.log(show)}
-              <li><a href="/#" onClick={(e) => setShow('contact')}>contact</a></li>
-              <li><a href="/#" onClick={(e) => setShow('resume')}>resume</a></li>
+              <li onClick={(e) => setShow('contact')}>contact</li>
+              <li onClick={(e) => setShow('resume')}>resume</li>
           </ul>
       </div>
 
@@ -30,7 +30,7 @@ function App() {
         <h2>About</h2>
         <p>Wat text eh boykes</p>
       </div>
-    </>
+    </Container>
   );
 }
 
