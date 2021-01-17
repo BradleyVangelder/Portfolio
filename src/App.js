aba2bc4a989741136133593ac1dee81a6adfd62b
bradley vangelder <r0784535@student.thomasmore.be>
2021-01-17 16:50:03 +0100

    response and color changes fixed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React, {useState} from 'react';
 import './App.css';
-import sea from "./resources/sea.mp4";
-import Container from 'react-bootstrap/Container'
 
 function App() {
 
   const [show, setShow] = useState('close');
 
   return (
-    <Container>
-      <video autoPlay loop muted id="videoBG">
-        <source src={sea} type="video/mp4"/>
-      </video>
-
+    <header className="website">
       <h1 className="animation_text animation">Bradley Vangelder</h1>
 
-      <h1 className="nav_name">Bradley Vangelder</h1>
+      <h2 className="nav_name">Bradley Vangelder</h2>
 
       <div className="nav">
           <ul className={show !== 'close' ? 'slide_left' : 'slide_back'}>
@@ -30,7 +24,7 @@ function App() {
         <h2>About</h2>
         <p>Wat text eh boykes</p>
       </div>
-    </Container>
+    </header>
   );
 }
 
