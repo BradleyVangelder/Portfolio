8b9de3065b27a92b53ec9162e475d80dceca209c
bradley vangelder <r0784535@student.thomasmore.be>
2021-02-12 12:47:42 +0100

    Changes to icons on contact page

--- src/App.js
+++ src/App.js
@@ -50,20 +50,20 @@ function App() {
           <h3>Contact</h3>
           <p>I'm open for some new experiences and projects to work on for a client. I'm active on different social media platforms so if you wanna contact me you should defenately try one of these.</p>
 
-          <div className="icons mt-2">
-              <a href="https://twitter.com/BradleyCreation" target="blank">
+          <p className="icons">
+              <a href="https://twitter.com/BradleyCreation">
               <i
               class="fab fa-twitter"></i>
               </a>
-              <a href="https://www.instagram.com/bradleyvangelder/" target="blank">
+              <a href="https://www.instagram.com/bradleyvangelder/">
                 <i
                 class="fab fa-instagram"></i>
               </a>
-              <a href="https://github.com/BradleyVangelder" target="blank">
+              <a href="https://github.com/BradleyVangelder">
                 <i
                 class="fab fa-github"></i>
               </a>
-          </div>
+          </p>
 
           <p className="back" onClick={() => setShow('close')}>
             <span class="material-icons">
