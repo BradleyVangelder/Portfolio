d051174f383359d413e18c80193ec5c5fff42eb5
bradley vangelder <r0784535@student.thomasmore.be>
2021-02-14 14:24:22 +0100

    changes to resume

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {useState} from 'react';
-import me from './pictures/me.jpg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 
@@ -38,7 +37,6 @@ function App() {
         <div className={show === 'about' ? 'slide_left_container container_modal container': 'none'}>
           <h3>About</h3>
           <p>I'm Bradley Vangelder, 19 Years, student at Thomas More Geel. In my free time i like to workout, play guitar, level my self up in graphic design and get just get some personal projects done. I'm a really social person and love to work in group to expand my contact portfolio.</p>
-          <img src={me} alt=""/>
           <p className="back" onClick={() => setShow('close')}>
             <span className="material-icons">
               west
@@ -48,8 +46,8 @@ function App() {
 
         <div className={show === 'contact' ? 'slide_left_container container_modal container': 'none'}>
           <h3>Contact</h3>
-          <p>I'm open for some new experiences and projects to work on for a client. I'm active on different social media platforms so if you wanna contact me you should defenately try one of these.</p>
-
+          <p className="text-contact">I'm open for some new experiences and projects to work on for a client. I'm active on different social media platforms so if you wanna contact me you should definitely try one of these:</p>
+          
           <p className="icons">
               <a href="https://twitter.com/BradleyCreation">
               <i
@@ -73,7 +71,7 @@ function App() {
         </div>
 
         <div className="resume-height">
-          <div className={show === 'resume' ? 'slide_left_container container_modal container resume': 'none'}>
+          <div className={show === 'resume' ? 'slide_left_container container_modal resume': 'none'}>
             <h3>Resume</h3>
             <div className="row">
               <div className="col-lg-6 col-sm-12">
@@ -90,8 +88,9 @@ function App() {
                 <p className="short_description">In my previous education, I gained experience by going on a foreign internship in Berlin for 3 weeks. During this project, I created the marketing and graphic design around an event they would be hosting in Spring 2021. Eventually I learned to work in group, a local graphic design team, also I learned to work to a client's standards but still bring my actual touch to the work.</p>
                 <p>LUANA LOUNGE
                 graphic design: company branding | 2019</p>
-                <p className="short_description">In my previous education, I gained experience by going on a foreign internship in Berlin for 3 weeks. During this project, I created the marketing and graphic design around an event they would be hosting in Spring 2021. Eventually I learned to work in group, a local graphic design team, also I learned to work to a client's standards but still bring my actual touch to the work.</p>
-                <a href="https://www.behance.net/gallery/113252385/Luana-Lounge-Branding">LINK TO PROJECT</a>
+                <p className="short_description">In my previous education, I gained experience by going on a foreign internship in Berlin for 3 weeks. During this project, I created the marketing and graphic design around an event they would be hosting in Spring 2021. Eventually I learned to work in group, a local graphic design team, also I learned to work to a client's standards but still bring my actual touch to the work.
+                </p>
+                <p><a href="https://www.behance.net/gallery/113252385/Luana-Lounge-Branding">LINK TO PROJECT</a></p>
                 <p>SNACK BAR FRITZ Studentjob</p>
                 <p className="short_description">For my student job I work at least 3 times during the week combined with the school work that comes with it. I work in the hospitality industry so I am among people a lot, which also makes me social and that I can deal well with people/customers. In this I learn that I have to be friendly at all times and I am faced with a lot of time pressure in this profession.</p>
               </div>
